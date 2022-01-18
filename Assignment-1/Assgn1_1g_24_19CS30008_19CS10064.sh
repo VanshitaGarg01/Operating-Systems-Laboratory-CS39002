ca69f346d78c6e8f0fde525b9d50f505393bd43d
ashutoshaks <ashu11.03.2002@gmail.com>
2022-01-18 11:10:13 +0530

    Modify g

--- Assignment-1/Assgn1_1g_24_19CS30008_19CS10064.sh
+++ Assignment-1/Assgn1_1g_24_19CS30008_19CS10064.sh
@@ -1,9 +1,5 @@
-for((i=0;i<150;i++))
+for((i=1;i<=150*10;i++))
 do 
-    for((j=0,str=$RANDOM;j<9;j++))
-    do
-        str="${str},$RANDOM"
-    done
-    echo $str>>$1
+    ((i%10==0))&&printf "$RANDOM\n">>$1||printf "$RANDOM,">>$1
 done
 awk -F, '$'$2'~/'$3'/{c++}END{print(c>0)?"YES":"NO"}' $1
--- Assignment-1/test.sh
+++ Assignment-1/test.sh
@@ -1,3 +1,5 @@
-file="test.txt"
-
-awk '$'$1'~/*/' $file
+for((i=1;i<=150*10;i++))
+do 
+    ((i%10==0))&&printf "$RANDOM\n">>$1||printf "$RANDOM,">>$1
+done
+awk -F, '$'$2'~/'$3'/{c++}END{print(c>0)?"YES":"NO"}' $1
