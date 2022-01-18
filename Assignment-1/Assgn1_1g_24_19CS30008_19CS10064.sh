206a9248f058986613d5fd5251973ce65131eb34
ashutoshaks <ashu11.03.2002@gmail.com>
2022-01-18 10:36:46 +0530

    Modify g

--- Assignment-1/Assgn1_1g_24_19CS30008_19CS10064.sh
+++ Assignment-1/Assgn1_1g_24_19CS30008_19CS10064.sh
@@ -1,12 +1,9 @@
-rm data.csv
-for i in {1..150}
+for((i=0;i<150;i++))
 do 
-    str=$RANDOM
-    for j in {1..9}
+    for((j=0,str=$RANDOM;j<9;j++))
     do
         str="${str},$RANDOM"
     done
-    echo $str >> $1
+    echo $str>>$1
 done
-
-awk -F, '$'$2'~/^[7]/ { c++ } END { if ($c>0) print "YES"; else print "NO" }' $1
+awk -F, '$'$2'~/'$3'/{c++}END{print(c>0)?"YES":"NO"}' $1
