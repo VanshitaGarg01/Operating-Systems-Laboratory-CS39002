28931f26c5fffbc41ba246bde58f863f9b352423
ashutoshaks <ashu11.03.2002@gmail.com>
2022-01-22 12:27:20 +0530

    Refactor

--- Assignment-1/Assgn1_1d_24_19CS30008_19CS10064.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p file_mod
-for file in data1d/*
-do
-    awk '{print NR,$0}' $file|sed 's/[[:blank:]]/,/g'>file_mod/${file##*/}
-done
--- Assignment-1/Assgn1_1g_24_19CS30008_19CS10064.sh
+++
@@ -1,5 +0,0 @@
-for((i=1;i<=150*10;i++))
-do 
-    ((i%10==0))&&printf "$RANDOM\n">>$1||printf "$RANDOM,">>$1
-done
-awk 'BEGIN{FS=","}$'$2'~/'$3'/{c++}END{print(c>0)?"YES":"NO"}' $1
